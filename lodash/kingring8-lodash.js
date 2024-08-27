f8ac4c7b49a54056d7abf7d498c554b392235950
king ring <ring2549214132@gmail.com>
2024-08-27 18:30:45 +0800

    a

--- lodash/kingring8-lodash.js
+++ lodash/kingring8-lodash.js
@@ -1,18 +1,17 @@
 
   var kingring8 = function() {
-    function compact1(array) {
+    function compact(array) {
       return array.filter(function(item) {
         return Boolean(item);
       });
     }
-  
-  
-
-  function chunk1(array,size){
-    const result=[];
-    for(let i=0;i<array.length;i+=size){
-      result.push(array.slice(i,i+size))
+    
+    function chunk(array, size) {
+      const result = [];
+      for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+      }
+      return result;
     }
-    return result;
-  }
+    
 }() 
