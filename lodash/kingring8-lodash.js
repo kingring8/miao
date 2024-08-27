ee5856c6875787c123e6bb3b8bf6bdb1dad12029
king ring <ring2549214132@gmail.com>
2024-08-27 17:44:58 +0800

    haowan

--- lodash/kingring8-lodash.js
+++ lodash/kingring8-lodash.js
@@ -1,17 +1,6 @@
-// 立即执行函数
-var kingring8 = (function () {
-    // 工具函数，根据数据类型，返回相应的函数
-    function iterater(iterater) {
-      if (typeof iterater === 'object' && iterater !== null) {
-        if (Array.isArray(iterater)) {
-          iterater = this.matchesProperty(iterater)
-        } else {
-          iterater = this.matches(iterater)
-        }
-      } else if (typeof iterater === 'number' || typeof iterater === 'string') {
-        iterater = this.property(iterater)
-      } else if (iterater === null) {
-        iterater = it => it
-      }
-      return iterater
-    }
+var kingring8 = function(){
+  function compact(array){
+    return array.filter(function(item){
+      return item !==false && item!== null&&item!==0&&item!==""&&item!==undefined&&item!==NaN;})
+  }
+}() 
